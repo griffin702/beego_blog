9b2d2b4b2ec509f04f140892ee25bb78638d6549
Griffin <117976509@qq.com>
2018-07-20 17:36:25 +0800

    处理分页ajax无刷新浏览

--- blog-master/static/themes/admin/js/admin.js
+++ blog-master/static/themes/admin/js/admin.js
@@ -85,4 +85,52 @@ $(document).ready(function(){
             });
         };
     });
+    //处理分页ajax
+    $("#wy-delegate-admin").on("click","ul.pagination li a",function(event){
+        event.preventDefault();
+        var ourl = $(this).attr('href');
+        var otitle = document.title;
+        if (ourl) {
+            ajax_Main("GET", {}, ourl);
+            if (history.pushState) {
+                var state = ({
+                    url: ourl, title: otitle
+                });
+                window.history.pushState(state, state.title, state.url);
+            } else {
+                window.location.href = "#!";
+            } // 如果不支持，使用旧的解决方案
+            return false;
+        }
+    });
+    //新增事件监听浏览器返回前进操作
+    window.addEventListener('popstate', function(e){
+        if (history.state){
+            //取出上一次状态
+            var state=e.state;
+            //修改当前标题为历史标题
+            document.title=state.title;
+            ajax_Main("GET",{},state.url);
+        }
+    }, false);
+    if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'manual';
+    }
 });
+
+function ajax_Main(type,data,url){
+    $.ajax({
+        type:type,
+        data:data,
+        url:url,
+        cache:true,
+        dataType:"html",
+        success: function(data){
+            $("table").html($(data).find("table"));
+            $(window).scrollTop(0);
+        },
+        error: function(){
+            alert("false");
+        }
+    });
+}
