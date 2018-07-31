64a81fa9876f5a4d11bd329e29a450fa6d0d9e71
Griffin <117976509@qq.com>
2018-07-31 17:02:47 +0800

    prettify代码高亮解决

--- blog-master/static/themes/admin/js/admin.js
+++ blog-master/static/themes/admin/js/admin.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
         plugins:{
             Code:{c:'btnCode',t:'插入代码',h:1,e:function(){
                     var _this=this;
-                    var htmlCode='<div><select id="xheCodeType"><option value="html">HTML/XML</option><option value="js">Javascript</option><option value="css">CSS</option><option value="php">PHP</option><option value="java">Java</option><option value="py">Python</option><option value="pl">Perl</option><option value="rb">Ruby</option><option value="cs">C#</option><option value="c">C++/C</option><option value="vb">VB/ASP</option><option value="">其它</option></select></div><div><textarea id="xheCodeValue" wrap="soft" spellcheck="false" style="width:300px;height:100px;" /></div><div style="text-align:right;"><input type="button" id="xheSave" value="确定" /></div>';
+                    var htmlCode='<div><select id="xheCodeType"><option value="go">Go</option><option value="py">Python</option><option value="html">HTML/XML</option><option value="js">Javascript</option><option value="css">CSS</option><option value="php">PHP</option><option value="java">Java</option><option value="pl">Perl</option><option value="rb">Ruby</option><option value="cs">C#</option><option value="c">C++/C</option><option value="vb">VB/ASP</option><option value="">其它</option></select></div><div><textarea id="xheCodeValue" wrap="soft" spellcheck="false" style="width:300px;height:100px;" /></div><div style="text-align:right;"><input type="button" id="xheSave" value="确定" /></div>';
                     var jCode=$(htmlCode),jType=$('#xheCodeType',jCode),jValue=$('#xheCodeValue',jCode),jSave=$('#xheSave',jCode);
                     jSave.click(function(){
                         _this.loadBookmark();
